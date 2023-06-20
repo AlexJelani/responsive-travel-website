f4b5f8a3f53b90bc696a1fff786d062c3c77c802
Jelani Alexander <jelanialexander82@gmail.com>
2023-06-20 19:36:19 +0900

    updated home php

--- home.php
+++ home.php
@@ -2,65 +2,88 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>home</title>
-    <!-- swiper css link -->
-    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+   <!-- swiper css link -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
-    <!-- font awesome cdn link  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-    <!-- custom css file link -->
-    <link rel="stylesheet" href="css/style.css">
+   <!-- custom css file link -->
+   <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <!-- header section starts -->
-    <section class="header">
-        <a href="home.php" class="logo">Travel</a>
-        <nav class="navbar">
-            <a href="home.php">home</a>
-            <a href="about.php">about</a>
-            <a href="package.php">package</a>
-            <a href="book.php">book</a>
-        </nav>
+   <!-- header section starts -->
+   <section class="header">
+      <a href="home.php" class="logo">Travel</a>
+      <nav class="navbar">
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="package.php">package</a>
+         <a href="book.php">book</a>
+      </nav>
 
-        <div id="menu-btn" class="fas fa-bars"></div>
-    </section>
+      <div id="menu-btn" class="fas fa-bars"></div>
+   </section>
 
 
-    <!-- header section ends -->
+   <!-- header section ends -->
 
-    <!-- home section starts  -->
-      <section class="home">
-         <div class="home-slider">
-            <div class="W">
-               <div class="slide" style="background:url(images/home-slide-1.jpg)no repeat">
-                  <div class="content">
-                     <span>explore, discover, travel</span>
-                     <h3>travel the world</h3>
-                     <a href="package.php" class="btn">discover more</a>
-                  </div>
-               </div>
-               <div class="slide" style="background:url(images/home-slide-1.jpg)no repeat">
-                  <div class="content">
-                     <span>explore, discover, travel</span>
-                     <h3>travel the world</h3>
-                     <a href="package.php" class="btn">discover more</a>
-                  </div>
+
+   <!-- home section starts  -->
+
+   <section class="home">
+
+      <div class="swiper home-slider">
+
+         <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+               <div class="content">
+                  <span>explore, discover, travel</span>
+                  <h3>travel arround the world</h3>
+                  <a href="package.php" class="btn">discover more</a>
                </div>
             </div>
-         </div>
-      </section>
 
-    <!-- home section ends  -->
+            <div class="swiper-slide slide" style="background:url(images/home-slide-2.jpg) no-repeat">
+               <div class="content">
+                  <span>explore, discover, travel</span>
+                  <h3>discover the new places</h3>
+                  <a href="package.php" class="btn">discover more</a>
+               </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+               <div class="content">
+                  <span>explore, discover, travel</span>
+                  <h3>make your tour worthwhile</h3>
+                  <a href="package.php" class="btn">discover more</a>
+               </div>
+            </div>
+
+         </div>
+
+         <div class="swiper-button-next"></div>
+         <div class="swiper-button-prev"></div>
+
+      </div>
+
+   </section>
+
+   <!-- home section ends -->
+
+
+   <!-- home section ends  -->
    <!-- services section starts  -->
    <section class="services">
       <h1 class="heading-title">our services</h1>
 
-      <dbiv class="box-container">
+      <div class="box-container">
          <div class="box">
             <img src="images/icon-1.png" alt="">
             <h3>adventures</h3>
@@ -85,62 +108,62 @@
             <img src="images/icon-6.png" alt="">
             <h3>camping</h3>
          </div>
-      </div>
+         </div>
    </section>
    <!-- services section ends  -->
-  
 
 
-<!-- footer section starts  -->
 
-<section class="footer">
+   <!-- footer section starts  -->
 
-   <div class="box-container">
+   <section class="footer">
 
-      <div class="box">
-         <h3>quick links</h3>
-         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
-         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
-         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
-         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      <div class="box-container">
+
+         <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+         </div>
+
+         <div class="box">
+            <h3>extra links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+         </div>
+
+         <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> jelanialexander@ymail.com </a>
+            <a href="#"> <i class="fas fa-map"></i> Gamagori, Japan - 400104 </a>
+         </div>
+
+         <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         </div>
+
       </div>
 
-      <div class="box">
-         <h3>extra links</h3>
-         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
-         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
-         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
-         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
-      </div>
+      <div class="credit"> created by <span>Jelani Alexander</span> | all rights reserved! </div>
 
-      <div class="box">
-         <h3>contact info</h3>
-         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
-         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
-         <a href="#"> <i class="fas fa-envelope"></i> jelanialexander@ymail.com </a>
-         <a href="#"> <i class="fas fa-map"></i> Gamagori, Japan - 400104 </a>
-      </div>
+   </section>
 
-      <div class="box">
-         <h3>follow us</h3>
-         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
-         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
-         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
-         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
-      </div>
+   <!-- footer section ends -->
 
-   </div>
-
-   <div class="credit"> created by <span>Jelani Alexander</span> | all rights reserved! </div>
-
-</section>
-
-<!-- footer section ends -->
-
-    <!-- swiper js link -->
-    <script src="swiper.php"></script>
-    <!-- custom js file link -->
-    <script src="js/script.js"></script>
+   <!-- swiper js link -->
+   <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+   <!-- custom js file link -->
+   <script src="js/script.js"></script>
 </body>
 
 </html>

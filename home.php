51e5467182a81a920d6a88b59e27cd3c7065eaf7
Jelani Alexander <jelanialexander82@gmail.com>
2023-06-27 09:44:34 +0900

    home packages added

--- home.php
+++ home.php
@@ -120,15 +120,59 @@
     </div>
     <div class="content">
         <h3>about us</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium libero quibusdam sed vel. Ab accusantium asperiores
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium libero quibusdam sed vel. Ab
+            accusantium asperiores
             laudantium natus placeat? At autem molestias numquam ratione repellendus sed suscipit temporibus vero?</p>
         <a href="about.php" class="btn">read more</a>
     </div>
 </section>
 
-
-
 <!--    home about section ends-->
+
+<!--    home packages section starts-->
+
+<section class="home-packages">
+    <h1 class="heading-title">our packages</h1>
+
+    <div class="box-container">
+        <div class="box">
+            <div class="image">
+                <img src="images/img-1.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/img-2.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. ?</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/img-3.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. ?</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="load-more">
+        <a href="package.php" class="btn">load more</a>
+    </div>
+</section>
+
+<!--    home packages section starts-->
 
 
 <!-- footer section starts  -->

b57d4bd09a9f2a74bb18450f8e6af16188464831
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-07-04 06:01:41 +0900

    amended package php

--- js/script.js
+++ js/script.js
@@ -34,3 +34,39 @@ var swiper = new Swiper(".reviews-slider", {
     },
   },
 });
+
+let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+let currentItem = 3;
+
+
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+  for(let i = currentItem; i < currentItem + 3; i++){
+  boxes[i].style.display = 'inline-block';
+  }
+  currentItem += 3;
+  if(currentItem >= boxes.length){
+    loadMoreBtn.style.display = 'none';
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
